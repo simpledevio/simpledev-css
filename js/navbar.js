fe575752605433cf08b53b21726b9f6c50bc55ea
Javier Alvarado <jdrummer1@outlook.com>
2023-09-03 00:53:15 -0700

    Remove old code from navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -4,10 +4,5 @@ let navMenu = document.querySelector('button');
 let navList = document.querySelector('.navbar ul');
 
 navMenu.addEventListener('click', function() {
-  // if (navList.style.display == 'none') {
-  //   navList.style.display = 'block';
-  // } else {
-  //   navList.style.display = 'none';
-  // }
   navList.classList.toggle('block');
 });
